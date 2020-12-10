13753fa8ce548ec186bf8fd7c1d6761a4194bb83
Matt Bongiorno <bigiorno@gmail.com>
2020-12-10 01:11:48 -0500

    app.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Container from '@material-ui/core/Container';
+import Directory from './components/Directory';
+
+function App() {
+    return ( <
+        Container >
+        <
+        Directory / >
+        <
+        /Container>
+    );
+}
+
+export default App;
